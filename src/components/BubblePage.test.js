d4f0495556fdded290d6a7af34dd3b5c2465c3b7
WillisLi <li.willis95@gmail.com>
2021-08-20 12:21:54 -0700

    cleaned up BubblePage testing file

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MutationObserver from 'mutationobserver-shim';
 
-import { render, screen, waitFor, act, fireEvent} from "@testing-library/react";
+import { render, screen, waitFor} from "@testing-library/react";
 import BubblePage from './BubblePage';
 import fetchColorService from './../services/fetchColorService';
 jest.mock("./../services/fetchColorService");
@@ -18,26 +18,13 @@ test("Renders without errors", ()=> {
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
-    await fetchColorService.mockResolvedValueOnce( 
-        [
-            { 
-                code: {hex: "#810040"},
-                color: "Maroon",
-                id: 1,
-            },
-            {
-                code: {hex: "#810050"},
-                color: "Maroon2",
-                id: 2, 
-            },
-        ]); 
+    fetchColorService.mockResolvedValueOnce(testColors); 
 
-    // render(<BubblePage />);  
-    await act( async () => render(<BubblePage/>));
+    render(<BubblePage />);  
 
     const colors = await screen.findAllByTestId("color");   
 
     await waitFor(() => { 
-        expect(colors).toHaveLength(2);
-    }) 
+        expect(colors).toHaveLength(1);
+    })
 }); 
